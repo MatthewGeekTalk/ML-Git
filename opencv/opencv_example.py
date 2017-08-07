bf843b4bbc2b8a66186551cda9d07b58cf7c7d59
Matthew <Matthew.he.geek@gmail.com>
2017-08-07 18:42:54 +0800

    change opencv example code

--- opencv/opencv_example.py
+++ opencv/opencv_example.py
@@ -1,13 +1,26 @@
 import cv2
+import numpy as np
+import copy
 
-video_capture = cv2.VideoCapture("768x576.avi")
+video_capture = cv2.VideoCapture("vedio.avi")
+foreGround = np.zeros((288, 384, 3), np.uint8)
+pBgModel = cv2.createBackgroundSubtractorMOG2()
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
-    if ret == False: 
+    if ret == False:
         break
-    cv2.imshow('Video', frame)
-
+    image = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+    fgmask = pBgModel.apply(image)
+    cv2.GaussianBlur(fgmask,(5,5),0)
+    cv2.threshold(fgmask,10,255,cv2.THRESH_BINARY)
+    foreGround = copy.copy(image)
+    foreGround = cv2.bitwise_and(foreGround, foreGround, mask = fgmask)
+    backGround = pBgModel.getBackgroundImage()
+    cv2.imshow('Video', image)
+    cv2.imshow("Background", backGround)
+    cv2.imshow("ForeGround", foreGround)
+    cv2.imshow("Foreground Mask", fgmask)
     # Hit 'q' on the keyboard to quit!
     k = cv2.waitKey(100) & 0xFF
     if k== 27:
