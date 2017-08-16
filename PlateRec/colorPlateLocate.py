98b01be9f68ce05d385c6bcc1eeb26f29f4f7a2e
zzfancitizen <zzfancitizen@gmail.com>
2017-08-17 00:18:45 +0800

    enlarge & rotation

--- PlateRec/colorPlateLocate.py
+++ PlateRec/colorPlateLocate.py
@@ -55,30 +55,84 @@ class ColorPlateLocate:
         self.min_white = min_white
         self.max_white = max_white
 
-    def plate_locate(self, img):
-        img = self.__set_bgr2hsv(img)
-        if self.case == "BLUE":
+    def __set_min_max(self, case):
+        if case == "BLUE":
             min_h = np.array([self.min_blue, 50, 50])
             max_h = np.array([self.max_blue, 255, 255])
-        elif self.case == "YELLOW":
+        elif case == "YELLOW":
             min_h = np.array([26, 43, 46])
             max_h = np.array([34, 255, 255])
-        elif self.case == "WHITE":
+        elif case == "WHITE":
             min_h = np.array([0, 0, 221])
             max_h = np.array([180, 30, 255])
         else:
             #     no input, using blue
             min_h = np.array([self.min_blue, 50, 50])
             max_h = np.array([self.max_blue, 255, 255])
+        return min_h, max_h
+
+    def plate_locate(self, img, case):
+        img = self.__set_bgr2hsv(img)
+        min_h, max_h = self.__set_min_max(case)
         img = cv2.inRange(img, min_h, max_h)
         img = self.__img_morph_close(img)
         cv2.imshow('img2', img)
         region = self.__find_plate_number_region(img)
-        self.plates = self.__detect_region(region, self.imgOrg)
+        plates = self.__detect_region(region, self.imgOrg)
+        plates = self.__split_plate(plates, case)
+        self.plates = plates
 
     def set_morph_hw(self, morph_w, morph_h):
         self.morphW = morph_w
         self.morphH = morph_h
+
+    def __split_plate(self, plates, case):
+        split_plates = []
+        for plate in plates:
+            img_org = plate.copy()
+            plate = self.__set_bgr2hsv(plate)
+            min_h, max_h = self.__set_min_max(case)
+            plate = cv2.inRange(plate, min_h, max_h)
+            plate = self.__img_morph_close(plate)
+            img_binary = plate.copy()
+            im2, contours, hierarchy = cv2.findContours(plate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            for i in range(len(contours)):
+                cnt = contours[i]
+                rect = cv2.minAreaRect(cnt)
+                height = int(rect[1][1])
+                width = int(rect[1][0])
+                if height == 0 or width == 0:
+                    continue
+                if self.__verify_value(height, width):
+                    continue
+                if self.__check_angle(height, width, rect[2]):
+                    continue
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                err, safe_rect = self.__calc_safe_rect(box, plate)
+                if not err:
+                    continue
+                safe_box = cv2.boxPoints(safe_rect)
+                safe_box = np.int0(safe_box)
+                ys = [safe_box[0, 1], safe_box[1, 1], safe_box[2, 1], safe_box[3, 1]]
+                xs = [safe_box[0, 0], safe_box[1, 0], safe_box[2, 0], safe_box[3, 0]]
+                ys_sorted_index = np.argsort(ys)
+                xs_sorted_index = np.argsort(xs)
+
+                x1 = safe_box[xs_sorted_index[0], 0]
+                x2 = safe_box[xs_sorted_index[3], 0]
+
+                y1 = safe_box[ys_sorted_index[0], 1]
+                y2 = safe_box[ys_sorted_index[3], 1]
+                # bi_plate = img_binary[y1:y2, x1:x2]
+                # bi_plate = bi_plate / 255
+                img_plate = img_org[y1:y2, x1:x2]
+                split_plates.append(img_plate)
+        return split_plates
+
+    @staticmethod
+    def __calc_parallelogram(self, img):
+        print(img)
 
     def set_verify_value(self, verify_min, verify_max, verify_aspect, verify_error):
         self.verify_min = verify_min
@@ -123,7 +177,7 @@ class ColorPlateLocate:
                 continue
             if self.__verify_value(height, width):
                 continue
-            if self.__check_angle(rect[1][0], rect[1][1], rect[2]):
+            if self.__check_angle(height, width, rect[2]):
                 continue
             err, safe_rect = self.__calc_safe_rect(box, img)
             if not err:
@@ -133,7 +187,7 @@ class ColorPlateLocate:
         return region
 
     @staticmethod
-    def __check_angle(width, height, angle):
+    def __check_angle(height, width, angle):
         if width / height < 1:
             angle = angle + 90
         if angle > 60 or angle < -60:
@@ -196,7 +250,7 @@ class ColorPlateLocate:
             box = cv2.boxPoints(rect)
             box = np.int0(box)
             # cv2.drawContours(self.imgOrg, [safe_box], 0, (0, 0, 255), 2)
-            # cv2.drawContours(self.imgOrg, [box], 0, (0, 255, 0), 2)
+            cv2.drawContours(self.imgOrg, [box], 0, (0, 255, 0), 2)
             ys = [safe_box[0, 1], safe_box[1, 1], safe_box[2, 1], safe_box[3, 1]]
             xs = [safe_box[0, 0], safe_box[1, 0], safe_box[2, 0], safe_box[3, 0]]
             ys_sorted_index = np.argsort(ys)
@@ -232,5 +286,5 @@ if __name__ == "__main__":
     plate_locate.set_img_hsv(255, 64, 95, 100, 140, 15, 40, 0, 30)
     plate_locate.set_verify_value(1, 200, 4, .5)
     plate_locate.set_morph_hw(10, 3)
-    plate_locate.plate_locate(my_img)
+    plate_locate.plate_locate(my_img, "BLUE")
     plate_locate.img_show()
