239087f1d9e760eec5b8caed34a3a9e8cf3c6fe1
Matthew <Matthew.he.geek@gmail.com>
2017-09-04 18:21:01 +0800

    Create TFrecords

---
+++ Plates/0/TFrecord_reader_0.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import numpy as np
+
+data_path = 'train.tfrecords_0'  # address to save the hdf5 file
+with tf.Session() as sess:
+    feature = {'train/image_0': tf.FixedLenFeature([], tf.string),
+               'train/label_0': tf.FixedLenFeature([], tf.int64)}
+    # Create a list of filenames and pass it to a queue
+    filename_queue = tf.train.string_input_producer([data_path], num_epochs=1)
+    # Define a reader and read the next record
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    # Decode the record read by the reader
+    features = tf.parse_single_example(serialized_example, features=feature)
+    # Convert the image data from string back to the numbers
+    image = tf.decode_raw(features['train/image_0'], tf.float32)
+
+    # Cast label data into int32
+    label = tf.cast(features['train/label_0'], tf.int32)
+    # Reshape image data into the original shape
+    image = tf.reshape(image, [224, 224, 3])
+
+    # Any preprocessing here ...
+
+    # Creates batches by randomly shuffling tensors
+    images, labels = tf.train.shuffle_batch([image, label], batch_size=448, capacity=30, num_threads=1,
+                                            min_after_dequeue=10)
+    print(images.shape)
+    print(labels.shape)
---
+++ Plates/1/TFrecord_writer_1.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+import sys
+from random import shuffle
+import glob
+
+shuffle_data = True
+rose_train_path = '*.jpg'
+addrs = glob.glob(rose_train_path)
+
+labels = []
+for i in range(len(addrs)):
+    labels.append(1) # 1 is not a plate
+
+if shuffle_data:
+    c = list(zip(addrs, labels))
+    shuffle(c)
+    addrs, labels = zip(*c)
+
+print(len(addrs))
+# train_addrs = addrs[0:int(0.6*len(addrs))]
+# train_labels = labels[0:int(0.6*len(labels))]
+# val_addrs = addrs[int(0.6*len(addrs)):int(0.8*len(addrs))]
+# val_labels = labels[int(0.6*len(addrs)):int(0.8*len(addrs))]
+# test_addrs = addrs[int(0.8*len(addrs)):]
+# test_labels = labels[int(0.8*len(labels)):]
+train_addrs = addrs
+train_labels = labels
+
+def load_image(addr):
+    # read an image and resize to (224, 224)
+    # cv2 load images as BGR, convert it to RGB
+    img = cv2.imread(addr)
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(np.float32)
+    return img
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+train_filename = 'train.tfrecords_1'  # address to save the TFRecords file
+# open the TFRecords file
+writer = tf.python_io.TFRecordWriter(train_filename)
+for i in range(len(train_addrs)):
+    # Load the image
+    img = load_image(train_addrs[i])
+    label = train_labels[i]
+    # Create a feature
+    feature = {'train/label_1': _int64_feature(label),
+               'train/image_1': _bytes_feature(tf.compat.as_bytes(img.tostring()))}
+    # Create an example protocol buffer
+    example = tf.train.Example(features=tf.train.Features(feature=feature))
+
+    # Serialize to string and write on the file
+    writer.write(example.SerializeToString())
+
+writer.close()
+sys.stdout.flush()
