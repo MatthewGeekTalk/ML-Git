739a7c44c091adbdfa37fdae6c1da1ed02f481b6
Matthew <Matthew.he.geek@gmail.com>
2017-09-13 19:23:47 +0800

    change image size to 20 72

--- PlateRec/binary_classification_CNN.py
+++ PlateRec/binary_classification_CNN.py
@@ -169,10 +169,10 @@ if __name__ == '__main__':
 
     accuracy = tf.reduce_mean(correct_prediction)
 
-    graph_location = tempfile.mkdtemp()
-    print('Saving  graph to: %s' % graph_location)
-    train_writer = tf.summary.FileWriter(graph_location)
-    train_writer.add_graph(tf.get_default_graph())
+    # graph_location = tempfile.mkdtemp()
+    # print('Saving  graph to: %s' % graph_location)
+    # train_writer = tf.summary.FileWriter(graph_location)
+    # train_writer.add_graph(tf.get_default_graph())
 
     path = os.path.abspath('./TFRecords')
     reader = tfrecords_reader(path)
