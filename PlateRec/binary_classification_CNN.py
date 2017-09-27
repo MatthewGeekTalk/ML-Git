9e50a7301aed3b5a3503ced871b5905e77618e33
zzfancitizen <zzfancitizen@gmail.com>
2017-09-27 16:25:17 +0800

    named cross entropy

--- PlateRec/binary_classification_CNN.py
+++ PlateRec/binary_classification_CNN.py
@@ -79,12 +79,12 @@ class deepcnn(object):
     @staticmethod
     def _weight_variable(shape):
         initial = tf.truncated_normal(shape, stddev=0.1)
-        return tf.Variable(initial, dtype=tf.float32,name='weight')
+        return tf.Variable(initial, dtype=tf.float32, name='weight')
 
     @staticmethod
     def _bias_variable(shape):
         initial = tf.constant(0.1, shape=shape)
-        return tf.Variable(initial, dtype=tf.float32,name='bias')
+        return tf.Variable(initial, dtype=tf.float32, name='bias')
 
     @staticmethod
     def _conv2d(x, W):
@@ -141,9 +141,9 @@ class deepcnn(object):
 
 
 if __name__ == '__main__':
-    x = tf.placeholder(tf.float32, [None, 20 * 70 * 3],name='x')
+    x = tf.placeholder(tf.float32, [None, 20 * 70 * 3], name='x')
     y_ = tf.placeholder(tf.float32, [None, 1], name='y_')
-    keep_prob = tf.placeholder(tf.float32,name='keep_prob')
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
     cnn = deepcnn(x)
 
@@ -157,9 +157,9 @@ if __name__ == '__main__':
     y_conv = cnn.build_cnn()
 
     with tf.name_scope(name='loss'):
-        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv)
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv, name="cross_entropy")
 
-    cross_entropy = tf.reduce_mean(cross_entropy)
+    cross_entropy = tf.reduce_mean(cross_entropy, name="reduce_ce")
 
     with tf.name_scope(name='adam_optimizer'):
         train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
