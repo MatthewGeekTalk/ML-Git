66b0deb3096f11705ef5b11b099539d5d20504a1
Matthew <Matthew.he.geek@gmail.com>
2017-10-09 14:08:36 +0800

    use tensorflow dataset

--- PlateRec/tool/tfrecords_reader.py
+++ PlateRec/tool/tfrecords_reader.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import os
+import tensorflow.contrib.data as tcd
 import matplotlib.pyplot as plt
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
@@ -11,20 +12,37 @@ class tfrecords_reader:
         # self.labels = []
         self.tfrecord_path = path
 
+    def _parse_function(self, example_proto):
+        keys_to_features = {'train/image':tf.FixedLenFeature([], tf.string),
+                          'train/label': tf.FixedLenFeature([2], dtype=tf.int64)}
+        features = tf.parse_single_example(example_proto, features=keys_to_features)
+        images = tf.decode_raw(features['train/image'], tf.uint8)
+        labels = tf.cast(features['train/label'], tf.int32)
+        images = tf.reshape(images, [20, 70, 3])
+        return images, labels
+
     def main(self, batch):
         features = self._load_tfrecords()
-        # imgs, labels = self._get_data_label(features, batch)
-        # return self._read_data(imgs, labels)
-        imgs, lbls = self._get_data_label(features, batch)
+        data_path = self.tfrecord_path + os.path.sep + 'plates.tfrecords'
+        dataset = tcd.TFRecordDataset(data_path)
+        dataset = dataset.map(self._parse_function)
+        dataset = dataset.shuffle(buffer_size=10000)
+        dataset = dataset.repeat()
+        dataset = dataset.batch(batch)
+        iterator = dataset.make_one_shot_iterator()
+        image_batch, label_batch  = iterator.get_next()
+        print(image_batch,label_batch)
+        # imgs, lbls = self._get_data_label(features, batch)
         with tf.Session() as sess:
-            # init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+            init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
             # init_op = tf.global_variables_initializer()
-            # sess.run(init_op)
+            sess.run(init_op)
             coord = tf.train.Coordinator()
             threads = tf.train.start_queue_runners(coord=coord)
-            images, labels = sess.run([imgs, lbls])
+            images, labels = sess.run([image_batch, label_batch])
             coord.request_stop()
             coord.join(threads)
+
         return images, labels
 
     def _load_tfrecords(self):
@@ -79,7 +97,8 @@ class tfrecords_reader:
 if __name__ == '__main__':
     path = os.path.abspath('../TFRecords')
     reader = tfrecords_reader(path)
-    imgs, labels = reader.main(2267)
+    imgs, labels = reader.main(50)
+    # imgs, labels = reader.main(3137)
     print(imgs.shape,labels.shape)
-    plt.imshow(imgs[2])
-    plt.show()
+    # plt.imshow(imgs[2])
+    # plt.show()
