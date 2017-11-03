2080ebeb8a08ba9fce2c9f916f48e62efb8efa62
zzfancitizen <zzfancitizen@gmail.com>
2017-11-03 17:36:55 +0800

    char determin model update

--- PlateRec/plateRec.py
+++ PlateRec/plateRec.py
@@ -119,6 +119,7 @@ class PlateRec(object):
         self._regions_sobel = object
         self._plates_color = object
         self._regions_color = object
+        self._plates_color_ori = object
 
         self._char_imgs = object
         self._char_labels = object
@@ -130,12 +131,18 @@ class PlateRec(object):
     def main(self):
         self._plates_sobel, self._regions_sobel, self._plates_sobel_ori = self.__detect_plate_sobel()
         self._plates_sobel_ori = self.__resize_plates(imgs=self._plates_sobel_ori)
-        # self._plates_color, self._regions_color = self.__detect_plate_color()
+        self._plates_color, self._regions_color, self._plates_color_ori = self.__detect_plate_color()
+        self._plates_color_ori = self.__resize_plates(imgs=self._plates_color_ori)
         self._img_con_sobel = self.__prepare_contours_img(regions=self._regions_sobel)
-        # self._img_con_color = self.__prepare_contours_img(regions=self._regions_color)
+        self._img_con_color = self.__prepare_contours_img(regions=self._regions_color)
 
-        for i in range(len(self._plates_sobel_ori)):
-            self.__detect_char(self._plates_sobel_ori[i])
+        if len(self._plates_sobel_ori) == len(self._plates_color_ori):
+            is_plates = self._plates_color_ori
+        else:
+            is_plates = self._plates_sobel_ori
+
+        for i in range(len(is_plates)):
+            self.__detect_char(is_plates[i])
 
     def __resize_plates(self, imgs):
         in_imgs = []
@@ -207,6 +214,7 @@ class PlateRec(object):
         plate_color.plate_locate(img, "BLUE")
         color_plates = plate_color.return_plates()
         color_regions = plate_color.return_regions()
+        color_plates_ori = plate_color.return_plates_ori()
 
         plate_validate = PlateValidate()
         imgs, labels = plate_validate.main(color_plates)
@@ -216,7 +224,7 @@ class PlateRec(object):
                 img_plate.append(imgs[i])
                 region_plate.append(color_regions[i])
 
-        return img_plate, region_plate
+        return img_plate, region_plate, color_plates_ori
 
     @staticmethod
     def __draw_contours(img, plate_regions):
@@ -282,6 +290,10 @@ class PlateRec(object):
     def plates_sobel_ori(self):
         return self._plates_sobel_ori
 
+    @property
+    def plates_color_ori(self):
+        return self._plates_color_ori
+
 
 if __name__ == '__main__':
 
@@ -297,10 +309,14 @@ if __name__ == '__main__':
     for plate in plate_rec.plates_sobel_ori:
         plate_rec.print_plate(plate)
 
+    for plate in plate_rec.plates_color_ori:
+        plate_rec.print_plate(plate)
+
         # path2 = input('Please input your saving path:')
         # plate_rec.save_plate(path2, plate)
 
     plate_rec.print_plate(plate_rec.img_con_sobel)
+    plate_rec.print_plate(plate_rec.img_con_color)
     print(plate_rec.plate_string)
 
     # for plate in plate_rec.plate_with_no:
